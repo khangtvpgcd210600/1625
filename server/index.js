0189e3ffcca6b27ad7abec90848bc7a32ba6f834
khangtvpgcd210600 <khangtvpgcd210600@fpt.edu.vn>
2023-06-20 23:41:45 +0700

    add client

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-mongoose.connect("mongodb+srv://longtngcd210014:0768104432long@cluster0.pd81jsy.mongodb.net/1925?retryWrites=true&w=majority");
+app.use(express.json());
+app.use(cors());
 
-app.listen(3001, () => {
-    console.log("Server run ok");
+mongoose.connect("mongodb+srv://longtngcd210014:0768104432long@cluster0.pd81jsy.mongodb.net/1925?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Successfully connected to MongoDB");
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
+    });
+}).catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
 });
